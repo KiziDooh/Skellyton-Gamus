2a59d48fd61675eae160b39f6dc9ff8d794aa579
KiziDooh <131369977+KiziDooh@users.noreply.github.com>
2025-04-14 05:57:37 -0500

    boop

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,5 +1,11 @@
 extends Node
 
+var blood = 100
+var soul = 0
+var bones = 0
+var valor = 0
+var faze = 0
+
 var px = 0
 var pz = 0
 var py = 0
@@ -7,5 +13,6 @@ var py = 0
 var sx = 0
 var sz = 0
 var sy = 0
+
 var get = 0
 var yup = false
